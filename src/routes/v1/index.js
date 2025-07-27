44032263484509640b27a0a7c4ed8f6ed49ee7ea
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-07-27 22:07:32 +0530

    Fix health check route path in API version 1 router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,7 +5,7 @@ import healthCheckRouter from './healthCheck.routes.js';
 const v1Router = express.Router();
 
 v1Router.use('/auth', authRouter);
-v1Router.use('/', healthCheckRouter);
+v1Router.use('/health-check', healthCheckRouter);
 
 
 export default v1Router;
