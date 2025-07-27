2ff3829eedce602c03280483515c49437557b089
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-07-27 22:07:27 +0530

    Add registration route to authentication API

--- src/routes/v1/auth.routes.js
+++ src/routes/v1/auth.routes.js
@@ -7,9 +7,12 @@
  */
 
 import express from 'express';
+import { register } from '../../controllers/auth.controller.js';
 
 
 const authRouter = express.Router();
+
+authRouter.post('/register', register);
 
 
 
